60db021f6904c0f73d0d035a63700d70c6083536
NetherPanther <53992987+Kpollock1@users.noreply.github.com>
2024-12-09 12:45:42 +0200

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(number, number2) {
+    return number + number2;
+}
+
+function subtract(number, number2) {
+    return number - number2;
+}
+
+function multiply(number, number2) {
+    return number * number2;
+}
+
+function divide(number, number2) {
+    return number / number2;
+}
