24eda7e789d82e9b714ecd9334ecaf7a56d19984
NetherPanther <53992987+Kpollock1@users.noreply.github.com>
2024-12-09 16:49:57 +0200

    Get values from click

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(number, number2) {
 function divide(number, number2) {
     return number / number2;
 }
+
+let firstNumber = 0
+let secondNumber = 0
+let operatorSign = ''
+
+function operate(number, operatorSign, number2) {
+
+}
+
+const container = document.getElementById('container');
+
+container.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (isButton) {
+        console.log(event.target.textContent);
+    }
+})
