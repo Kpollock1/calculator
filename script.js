4bf9733814ba5e44f24d641fc569fe56db34cec1
NetherPanther <53992987+Kpollock1@users.noreply.github.com>
2024-12-09 20:23:41 +0200

    Make calculator operate

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(number, number2) {
-    return number + number2;
+    return Number(number) + Number(number2);
 }
 
 function subtract(number, number2) {
@@ -14,19 +14,61 @@ function divide(number, number2) {
     return number / number2;
 }
 
-let firstNumber = 0
-let secondNumber = 0
+let firstNumberArr = []
+let secondNumberArr = []
 let operatorSign = ''
+let state = 'left'
+let firstNumberJoined = firstNumberArr.join('')
+let secondNumberJoined = secondNumberArr.join('')
 
-function operate(number, operatorSign, number2) {
-
+function operate(number, number2) {
+    if (operatorSign === "+") {
+        return add(number, number2);
+    } else if (operatorSign === "-") {
+        return subtract(number, number2);
+    } else if (operatorSign === "*") {
+        return multiply(number, number2);
+    } else if (operatorSign === "/") {
+        return divide(number, number2);
+    }
 }
-
+const display = document.getElementById('displayScreen')
 const container = document.getElementById('container');
 
 container.addEventListener('click', (event) => {
     const isButton = event.target.nodeName === 'BUTTON';
-    if (isButton) {
-        console.log(event.target.textContent);
+    // add number to array firstNumberArr
+    if (state === 'left' && isButton && !isNaN(Number(event.target.textContent))) {
+        firstNumberArr.push(event.target.textContent);
+        display.textContent = firstNumberArr.join('');
     }
+
+    if (state === 'right' && isButton && !isNaN(Number(event.target.textContent))) {
+        secondNumberArr.push(event.target.textContent);
+        display.textContent = secondNumberArr.join('');
+    }
+
+    // flip state to 'right'
+    if (isButton && (event.target.textContent === '+'
+        || event.target.textContent === '-'
+        || event.target.textContent === '*'
+        || event.target.textContent === '/')) {
+        operatorSign = event.target.textContent;
+        display.textContent = event.target.textContent;
+        state = 'right';
+    }
+    // clear screen
+    if (isButton && event.target.textContent === 'AC') {
+        firstNumberArr = []
+        secondNumberArr = []
+        operatorSign = ''
+        display.textContent = '';
+        state = 'left';
+    }
+
+    if (isButton && event.target.textContent === '=') {
+        display.textContent = operate(firstNumberArr.join(''), secondNumberArr.join(''), '-');
+    }
+
+
 })
